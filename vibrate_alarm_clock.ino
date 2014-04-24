1b9ff6373c0b57e3d859ef579a16ccaaea3c94d4
Yeo Kheng Meng <yeokm1@gmail.com>
2014-04-24 16:56:13 +0800

    formatting

--- vibrate_alarm_clock.ino
+++ vibrate_alarm_clock.ino
@@ -8,13 +8,13 @@
 
 #define BUTTON_ALARM_SET_PIN A4
 #define BUTTON_TIME_SET_PIN A5
+#define LCD_OFF_PIN 6 //D6
 
 #define MOTOR_PIN1 A3
 #define MOTOR_PIN2 A2
 #define MOTOR_SLEEP_PIN 7 //D7
 
 #define SPEAKER_PIN 9 //D9
-#define LCD_OFF_PIN 6 //D6
 
 #define RXLED_PIN 17
 #define OLED_RESET_PIN A0
