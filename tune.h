4ea4e9f3dcc2c2e254e40391923b89a73c5e3df5
Yeo Kheng Meng <yeokm1@gmail.com>
2014-05-11 11:15:49 +0800

    tune optimisation

--- tune.h
+++ tune.h
@@ -1,63 +1,44 @@
 #include <avr/pgmspace.h>
 #include "pitches.h"
 
-#define NUM_TONES 60
+#define NUM_TONES 78
 //Mario tone from http://www.linuxcircle.com/2013/03/31/playing-mario-bros-tune-with-arduino-and-piezo-buzzer/
 
 PROGMEM prog_uint16_t melody[] = {
+  
+  //0
   NOTE_E7, NOTE_E7, 0, NOTE_E7, 
   0, NOTE_C7, NOTE_E7, 0,
   NOTE_G7, 0, 0,  0,
   NOTE_G6, 0, 0, 0, 
 
+  //16
   NOTE_C7, 0, 0, NOTE_G6, 
   0, 0, NOTE_E6, 0, 
   0, NOTE_A6, 0, NOTE_B6, 
   0, NOTE_AS6, NOTE_A6, 0, 
 
-  NOTE_G6, NOTE_E7, NOTE_G7, 
+  //32
+  NOTE_G6, NOTE_E7, NOTE_G7, //Tones on this line have shorter duration
   NOTE_A7, 0, NOTE_F7, NOTE_G7, 
   0, NOTE_E7, 0,NOTE_C7, 
   NOTE_D7, NOTE_B6, 0, 0,
 
+  //47
   NOTE_C7, 0, 0, NOTE_G6, 
   0, 0, NOTE_E6, 0, 
   0, NOTE_A6, 0, NOTE_B6, 
   0, NOTE_AS6, NOTE_A6, 0, 
 
-  NOTE_G6, NOTE_E7, NOTE_G7, 
+  //63
+  NOTE_G6, NOTE_E7, NOTE_G7,  //Tones on this line have shorter duration
   NOTE_A7, 0, NOTE_F7, NOTE_G7, 
   0, NOTE_E7, 0,NOTE_C7, 
   NOTE_D7, NOTE_B6, 0, 0
+
+  //78
 };
 
-
-PROGMEM prog_uint16_t noteDurations[] = {
-  12, 12, 12, 12, 
-  12, 12, 12, 12,
-  12, 12, 12, 12,
-  12, 12, 12, 12, 
-
-  12, 12, 12, 12,
-  12, 12, 12, 12, 
-  12, 12, 12, 12, 
-  12, 12, 12, 12, 
-
-  9, 9, 9,
-  12, 12, 12, 12,
-  12, 12, 12, 12,
-  12, 12, 12, 12,
-
-  12, 12, 12, 12,
-  12, 12, 12, 12,
-  12, 12, 12, 12,
-  12, 12, 12, 12,
-
-  9, 9, 9,
-  12, 12, 12, 12,
-  12, 12, 12, 12,
-  12, 12, 12, 12,
-};
 
 
 int getToneAtThisPosition(int position){
@@ -65,8 +46,15 @@ int getToneAtThisPosition(int position){
 }
 
 int getDurationAtThisPosition(int position){
-  return pgm_read_word_near(noteDurations + position);
+  if((32 <= position && position <= 34)
+      || (63 <= position && position <= 65)){
+        return 9;
+  } else {
+    return 12;
+  }
+
 }
+
 
 
 
